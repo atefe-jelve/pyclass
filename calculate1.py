d481e08fe650c0fa0ade7221fa463163bd1bc11f
atefe-jelve <88873821+atefe-jelve@users.noreply.github.com>
2021-08-26 18:11:18 +0430

    Add files via upload

---
+++ calculate1.py
@@ -0,0 +1,13 @@
+#ATEFE-JELVE 5SHANBE 2-6
+#calculate 2 number WHIT OPERATOR
+first = float(input('please enter first number:\n'))
+secend = float(input('please enter secend number:\n'))
+operator = (input('please enter operator:(+ , - , * or /)\n'))
+if operator == '+' :
+    print(first+secend)
+if operator == '-' :
+    print(first-secend)
+if operator == '*' :
+    print(first*secend)
+if operator == '/' :
+    print(first/secend)
